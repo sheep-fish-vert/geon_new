5147a3efb879bdcf1e0f353554b048c62f839f1c
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-05-17 12:23:15 +0300

    qwe

--- src/js/develop/develop_5.js
+++ src/js/develop/develop_5.js
@@ -1,8 +1,30 @@
+function tabsFour() {
+    var tab = $('.four .tab');
 
+    var info = $('.four .info');
+    tab.each(function () {
+       if($(this).hasClass('active')){
+           var ind = $(this).index();
+           info.eq(ind).stop().slideDown();
+       }
+    });
+
+}
+function tabClick() {
+    var tab = $('.four .tab');
+    var info = $('.four .info');
+    tab.click(function () {
+       tab.removeClass('active');
+        $(this).addClass('active');
+        info.stop().slideUp();
+        tabsFour();
+    });
+}
 
 
 $(document).ready(function(){
-
+    tabsFour();
+    tabClick();
 });
 
 $(window).load(function(){
