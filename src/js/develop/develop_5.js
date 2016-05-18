c6e3af4d8e3e6cc2e2b3e383f306cb87f83cc2be
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-05-18 11:32:07 +0300

    qweqwe

--- src/js/develop/develop_5.js
+++ src/js/develop/develop_5.js
@@ -28,11 +28,56 @@ $('.slick-slider-six').slick({
     arrow:true
 });
 }
+function tabsSeven() {
+    var tab = $('.seven .tab');
+    var info = $('.seven .info');
+    tab.click(function () {
+        if(!$(this).hasClass('active')){
+            tab.removeClass('active');
+            info.removeClass('active');
+            var ind = $(this).index();
+            $(this).addClass('active');
+            info.eq(ind).addClass('active');
+        }
+    });
+}
+function arrowsSevenTabs() {
+    var prev = $('.seven .prev');
+    var next = $('.seven .next');
+    var tab = $('.seven .tab');
+    var info = $('.seven .info');
+    prev.click(function () {
+        var current = $('.seven .tab.active').index();
+        if(current == 0){
+            current = tab.length - 1;
+        }else{
+            current--;
+        }
+        reAddClass(current);
 
+    });
+    next.click(function () {
+        var current = $('.seven .tab.active').index();
+        if(current == tab.length - 1){
+            current = 0;
+        }else{
+            current++;
+        }
+        reAddClass(current);
+    });
+    function reAddClass(current) {
+        tab.removeClass('active');
+        info.removeClass('active');
+        info.eq(current).addClass('active');
+        tab.eq(current).addClass('active');
+    }
+}
 $(document).ready(function(){
     tabsFour();
     tabClick();
     sliderInitSix();
+    tabsSeven();
+    arrowsSevenTabs();
 });
 
 $(window).load(function(){
